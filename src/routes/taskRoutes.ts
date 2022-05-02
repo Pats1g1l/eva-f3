f38df652011e05abfae2da5e0cd6f63caaf002c0
Patricio Almonacid <pat.nas@outlook.es>
2022-05-02 00:25:26 -0400

    Primer Commit Evaluacion 3

---
+++ src/routes/taskRoutes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express" 
+import TaskControllers from "../controllers/TaskControllers"
+
+const taskRoutes = Router()
+const controller = new TaskControllers()
+
+taskRoutes.get('/', controller.getAll)
+taskRoutes.get('/:id', controller.getById)
+taskRoutes.post('/', controller.create)
+taskRoutes.put('/:id', controller.update)
+taskRoutes.delete('/:id', controller.delete)
+
+export default taskRoutes
